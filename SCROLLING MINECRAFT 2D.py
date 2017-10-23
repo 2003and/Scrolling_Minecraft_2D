c33136a8fe303bd0b7897ecdebdeb2d582b6dfdb
Andrew Pavin <2003and@gmail.com>
2017-10-23 21:34:40 +0300

    I tried to make the clouds work, but it's not that easy. Guess I'll fix their rendering in the next version

--- SCROLLING MINECRAFT 2D.py
+++ SCROLLING MINECRAFT 2D.py
@@ -127,13 +127,14 @@ def render_entities():
                                   settings.tilesize - settings.playersize])
     for r in range(coeff_y, coeff_y + settings.maxfity + 1):
         for c in range(coeff_x, coeff_x + settings.maxfitx + 1):
-            if 0 <= r < settings.mapheight and 0 <= c < settings.mapwidth:
-                if small_entity_field[r][c] > 0:
-                    pg.draw.rect(screen, colormap[small_entity_field[r][c]],
-                                 [settings.tilesize * (c - coeff_x) + (settings.playersize // 3 * 2) * 1.5,
-                                  settings.tilesize * (r - coeff_y) + (settings.playersize // 3 * 2) * 1.5,
-                                  settings.tilesize - (settings.playersize // 3 * 2) * 3,
-                                  settings.tilesize - (settings.playersize // 3 * 2) * 3])
+            pass
+        if 0 <= r < settings.mapheight and 0 <= c < settings.mapwidth:
+            if small_entity_field[r][c] > 0:
+                pg.draw.rect(screen, colormap[small_entity_field[r][c]],
+                             [settings.tilesize * (c - coeff_x) + (settings.playersize // 3 * 2) * 1.5,
+                              settings.tilesize * (r - coeff_y) + (settings.playersize // 3 * 2) * 1.5,
+                              settings.tilesize - (settings.playersize // 3 * 2) * 3,
+                              settings.tilesize - (settings.playersize // 3 * 2) * 3])
 
 
 def render_clouds():
@@ -141,7 +142,7 @@ def render_clouds():
         if cloudx[i] > settings.mapwidth * settings.tilesize + settings.cloudwidth:
             cloudx[i] = settings.cloudwidth * -1
             cloudy[i] = random.randint(0, settings.mapheight * settings.tilesize)
-        pg.draw.rect(screen, colors.white, [cloudx[i], cloudy[i], settings.cloudwidth, settings.cloudheight], 10)
+        pg.draw.rect(screen, colors.white, [cloudx[i]+coeff_x, cloudy[i]+coeff_y, settings.cloudwidth, settings.cloudheight], 10)
         cloudx[i] += i + 1
 
 
@@ -187,7 +188,7 @@ def render_inventory():
             text = font.render(str(inventory[resources[i]]), False, colors.black, colors.white)
             screen.blit(text,
                         [x_pos, settings.maxfity * settings.tilesize + settings.tilesize + settings.tilesize // 3])
-            # render_clouds()
+            render_clouds()
 
 
 def render_player():
